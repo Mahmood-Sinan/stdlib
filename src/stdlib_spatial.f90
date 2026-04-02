c89dbe714acdbaab60930dc20db63637ffd34eda
Mahmood-Sinan <sinanmahmood7@gmail.com>
2026-04-02 17:47:07 +0000

    Deploying to stdlib-fpm from @ Mahmood-Sinan/stdlib@a9f0fc19d759e54d7fa2c6424a9ed6a5491129d3 🚀

--- src/stdlib_spatial.f90
+++ src/stdlib_spatial.f90
@@ -6,90 +6,84 @@ module stdlib_spatial
     public :: kabsch_umeyama
 
     interface kabsch_umeyama
-        !-----------------------------------------------------------------------
-        !> Compute the optimal similarity transform (Kabsch–Umeyama):
-        !>
-        !>     P ≈ c * R * Q + t
-        !>
-        !> where:
-        !>   - R is an orthogonal rotation matrix
-        !>   - c is an optional scale factor
-        !>   - t is a translation vector
-        !>
-        !> The transformation minimizes the RMSD between corresponding columns
-        !> of P and Q, optionally using weights.
-        !-----------------------------------------------------------------------
+        !! ([Specifications](../page/specs/stdlib_spatial.html#kabsch_umeyama))
+        !! This interface computes the optimal similarity transform (Kabsch–Umeyama):
+        !! \[
+        !!      P \approx c \, R \, Q + t
+        !! \]
+        !! The transformation minimizes the RMSD between corresponding columns
+        !! of P and Q, optionally using weights and with optional scaling.
         module subroutine kabsch_umeyama_sp(P, Q, R, t, c, rmsd, W, scale)
-            !> Target point set (d × N)
             real(sp), intent(in) :: P(:, :)
-            !> Reference point set (d × N)
+            !! Target point set (d × N)
             real(sp), intent(in) :: Q(:, :)
-            !> Optimal rotation matrix (d × d)
+            !! Reference point set (d × N)
             real(sp), intent(out) :: R(:, :)
-            !> Translation vector (d)
+            !! Optimal rotation matrix (d × d)
             real(sp), intent(out) :: t(:)
-            !> Scale factor
+            !! Translation vector (d)
             real(sp), intent(out) :: c
-            !> Root-mean-square deviation
+            !! Scale factor
             real(sp), intent(out) :: rmsd
-            !> Optional weights
+            !! Root-mean-square deviation
             real(sp), intent(in), optional :: W(:)
-            !> Enable scaling
+            !! Optional weights
             logical, intent(in), optional :: scale
+            !! Enable scaling
         end subroutine
         module subroutine kabsch_umeyama_dp(P, Q, R, t, c, rmsd, W, scale)
-            !> Target point set (d × N)
             real(dp), intent(in) :: P(:, :)
-            !> Reference point set (d × N)
+            !! Target point set (d × N)
             real(dp), intent(in) :: Q(:, :)
-            !> Optimal rotation matrix (d × d)
+            !! Reference point set (d × N)
             real(dp), intent(out) :: R(:, :)
-            !> Translation vector (d)
+            !! Optimal rotation matrix (d × d)
             real(dp), intent(out) :: t(:)
-            !> Scale factor
+            !! Translation vector (d)
             real(dp), intent(out) :: c
-            !> Root-mean-square deviation
+            !! Scale factor
             real(dp), intent(out) :: rmsd
-            !> Optional weights
+            !! Root-mean-square deviation
             real(dp), intent(in), optional :: W(:)
-            !> Enable scaling
+            !! Optional weights
             logical, intent(in), optional :: scale
+            !! Enable scaling
         end subroutine
         module subroutine kabsch_umeyama_csp(P, Q, R, t, c, rmsd, W, scale)
-            !> Target point set (d × N)
             complex(sp), intent(in) :: P(:, :)
-            !> Reference point set (d × N)
+            !! Target point set (d × N)
             complex(sp), intent(in) :: Q(:, :)
-            !> Optimal rotation matrix (d × d)
+            !! Reference point set (d × N)
             complex(sp), intent(out) :: R(:, :)
-            !> Translation vector (d)
+            !! Optimal rotation matrix (d × d)
             complex(sp), intent(out) :: t(:)
-            !> Scale factor
+            !! Translation vector (d)
             complex(sp), intent(out) :: c
-            !> Root-mean-square deviation
+            !! Scale factor
             real(sp), intent(out) :: rmsd
-            !> Optional weights
+            !! Root-mean-square deviation
             real(sp), intent(in), optional :: W(:)
-            !> Enable scaling
+            !! Optional weights
             logical, intent(in), optional :: scale
+            !! Enable scaling
         end subroutine
         module subroutine kabsch_umeyama_cdp(P, Q, R, t, c, rmsd, W, scale)
-            !> Target point set (d × N)
             complex(dp), intent(in) :: P(:, :)
-            !> Reference point set (d × N)
+            !! Target point set (d × N)
             complex(dp), intent(in) :: Q(:, :)
-            !> Optimal rotation matrix (d × d)
+            !! Reference point set (d × N)
             complex(dp), intent(out) :: R(:, :)
-            !> Translation vector (d)
+            !! Optimal rotation matrix (d × d)
             complex(dp), intent(out) :: t(:)
-            !> Scale factor
+            !! Translation vector (d)
             complex(dp), intent(out) :: c
-            !> Root-mean-square deviation
+            !! Scale factor
             real(dp), intent(out) :: rmsd
-            !> Optional weights
+            !! Root-mean-square deviation
             real(dp), intent(in), optional :: W(:)
-            !> Enable scaling
+            !! Optional weights
             logical, intent(in), optional :: scale
+            !! Enable scaling
         end subroutine
     end interface
 end module stdlib_spatial
